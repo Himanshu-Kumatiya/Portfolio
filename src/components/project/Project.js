0745bf43b8b27a3f9ed833060685fc54bd36c4c5
Himanshu kumatiya <kumatiya123@gmail.com>
2024-07-26 16:53:11 +0530

    favicon changed

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,26 +1,29 @@
 import { useRef } from "react";
 import "./project.scss";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
-
+import woodhill from "../../assests/Woodhill.png"
+import BlogPost from "../../assests/BlogPost.png"
+import knowledgeGrasp from "../../assests/KnowledgeGrasp.png"
 const items = [
   {
     id: 1,
-    title: "Knowledge Grasp",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    title: "KnowledgeGrasp",
+    img: knowledgeGrasp,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
   },
   {
     id: 2,
-    title: "BlogPost",
-    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    title: "Woodhill",
+    img: woodhill,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
   },
   {
     id: 3,
-    title: "Woodhill",
-    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    title: "BlogPost",
+    img: BlogPost,
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
   },
+  
 ];
 
 const Single = ({ item }) => {
